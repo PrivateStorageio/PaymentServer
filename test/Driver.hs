3964bcdfffc411bd4e34440a460f962725eb908e
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2019-09-04 15:40:26 -0400

    Revert to master Driver.hs, will revisit this later

--- test/Driver.hs
+++ test/Driver.hs
@@ -1,42 +1,8 @@
--- XXX
--- Generated with
---    $ rm test/Driver.hs && stack exec tasty-discover "./test/Driver.hs" . ./test/Driver.hs
--- Need to automate that somehow.
-{-# LINE 1 "./test/Driver.hs" #-}
-{-# LANGUAGE FlexibleInstances #-}
-module Main (main, ingredients, tests) where
-import Prelude
-import qualified System.Environment as E
-import qualified Test.Tasty as T
-import qualified Test.Tasty.Ingredients as T
-import qualified Test.Tasty.QuickCheck as QC
+{-# OPTIONS_GHC -F -pgmF tasty-discover #-}
 
-import qualified Test.Tasty.Hspec as HS
-
-import qualified SpecPersistence
-
-import qualified SpecStripe
-
-import qualified SpecRedemption
-
-
-
-tests :: IO T.TestTree
-tests = do
-  t0 <- HS.testSpec "memory" SpecPersistence.spec_memory
-
-  t1 <- HS.testSpec "webhook" SpecStripe.spec_webhook
-
-  t2 <- pure $ QC.testProperty "getVoucherFindsVoucher" SpecStripe.prop_getVoucherFindsVoucher
-
-  t3 <- pure $ QC.testProperty "getVoucherWithoutVoucher" SpecStripe.prop_getVoucherWithoutVoucher
-
-  t4 <- HS.testSpec "redemption" SpecRedemption.spec_redemption
-
-  pure $ T.testGroup "./test/Driver.hs" [t0,t1,t2,t3,t4]
-ingredients :: [T.Ingredient]
-ingredients = T.defaultIngredients
-main :: IO ()
-main = do
-  args <- E.getArgs
-  E.withArgs ([] ++ args) $    tests >>= T.defaultMainWithIngredients ingredients
+-- This is a module where we can hang the above preprocessor definition to
+-- direct tasty-discover to find our test suite spread across the rest of the
+-- modules rooted in this directory.
+--
+-- See the test-suite definition in PaymentServer.cabal
+-- See also https://git.coop/decentral1se/tasty-discover
