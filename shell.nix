6291a2bf1391fa73cd5cc0d11cadbde5191a64fd
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2019-09-09 11:30:00 -0400

    avoid the recursive definition

--- shell.nix
+++ shell.nix
@@ -1,10 +1,10 @@
 let
-  pkgs = import (builtins.fetchTarball {
+  pinnedPkgs = import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/3c83ad6ac13b67101cc3e2e07781963a010c1624.tar.gz";
     sha256 = "0cdq342wrkvkyccygpp1gvwp7hhqg68hljjwld4vjixm901ayy14";
   }) {};
 in
-{ pkgs ? pkgs }:
+{ pkgs ? pinnedPkgs }:
 pkgs.mkShell {
   buildInputs = [
     pkgs.stack
