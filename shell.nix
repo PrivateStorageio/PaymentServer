5d996e2a53d2f4099dff4de9eae04f386bdb945a
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2019-09-10 15:30:59 -0400

    Try specifying the nixpkgs we want like this, instead.

--- shell.nix
+++ shell.nix
@@ -1,12 +1,6 @@
-let
-  pinnedPkgs = import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/3c83ad6ac13b67101cc3e2e07781963a010c1624.tar.gz";
-    sha256 = "0cdq342wrkvkyccygpp1gvwp7hhqg68hljjwld4vjixm901ayy14";
-  }) {};
-in
-{ ... }:
-pinnedPkgs.mkShell {
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.mkShell {
   buildInputs = [
-    pinnedPkgs.stack
+    pkgs.stack
   ];
 }
