a1be8e3eff82c3cd9bb232f8490df0eff3d27ef3
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2019-11-22 09:35:37 -0500

    Update Nix packaging to reference wai-cors dep

--- nix/PaymentServer.nix
+++ nix/PaymentServer.nix
@@ -64,6 +64,7 @@ in { system, compiler, flags, pkgs, hsPkgs, pkgconfPkgs, ... }:
           (hsPkgs."servant-server" or (buildDepError "servant-server"))
           (hsPkgs."wai" or (buildDepError "wai"))
           (hsPkgs."wai-extra" or (buildDepError "wai-extra"))
+          (hsPkgs."wai-cors" or (buildDepError "wai-cors"))
           (hsPkgs."data-default" or (buildDepError "data-default"))
           (hsPkgs."warp" or (buildDepError "warp"))
           (hsPkgs."warp-tls" or (buildDepError "warp-tls"))
