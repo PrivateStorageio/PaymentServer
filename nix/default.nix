e3176ab292c0d2dac9f35734e185344bba04f8dd
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2020-04-06 13:41:18 -0400

    Use the mkStackPkgSet example from the Haskell.nix readme

--- nix/default.nix
+++ nix/default.nix
@@ -1,18 +1,15 @@
-{ pkgs ? import <nixpkgs> { overlays = [ (import ./overlay.nix) ]; } }:
-
 let
   # Pin a particular version of haskell.nix.  The particular version isn't
   # special.  It's just recent at the time this expression was written and it
   # is known to work with PaymentServer.  It could be bumped if necessary but
   # this would probably only happen as a result of bumping the resolver in
   # stack.yaml.
-  haskell = import (builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/0cb32e695d7014908fb01fd7e3d225ea33dbdc98.tar.gz) { inherit pkgs; };
-
-  pkgSet = haskell.mkStackPkgSet {
-    stack-pkgs = import ./pkgs.nix;
-    pkg-def-extras = [];
-    modules = [];
-  };
-
+  haskellNix = import (builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/d57402225e125369805fb262f3d7497dabb8dc82.tar.gz) {};
+  nixpkgsSrc = haskellNix.sources.nixpkgs-1909;
+  nixpkgsArgs = haskellNix.nixpkgsArgs;
 in
-  pkgSet.config.hsPkgs
+{ pkgs ? import nixpkgsSrc (nixpkgsArgs // { overlays = nixpkgsArgs.overlays ++ [ (import ./overlay.nix) ]; })
+}:
+pkgs.haskell-nix.stackProject {
+  src = pkgs.haskell-nix.haskellLib.cleanGit { name = "PaymentServer"; src = ../.; };
+}
