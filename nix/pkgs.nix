354d174f641e2e433c72dde8eb12c9fbaeb875a4
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2020-09-21 11:29:00 -0400

    Regenerate Nix packaging

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -5,7 +5,10 @@
         "stripe-core" = (((hackage.stripe-core)."2.5.0").revisions).default;
         "stripe-haskell" = (((hackage.stripe-haskell)."2.5.0").revisions).default;
         "stripe-http-client" = (((hackage.stripe-http-client)."2.5.0").revisions).default;
-        } // { PaymentServer = ./PaymentServer.nix; }) // {};
+        } // {
+        PaymentServer = ./PaymentServer.nix;
+        servant-prometheus = ./servant-prometheus.nix;
+        }) // {};
       };
   resolver = "lts-14.1";
   }
