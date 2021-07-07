876f1628def049c94105bd81fec73511f96da235
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2021-07-07 10:15:43 -0400

    Update to newer Haskell.nix
    This doesn't fix anything yet but maybe running non-ancient versions will make
    it easier to fix something.

---
+++ default.nix
@@ -0,0 +1,31 @@
+let
+  # Read in the Niv sources
+  sources = import ./nix/sources.nix {};
+  # If ./nix/sources.nix file is not found run:
+  #   niv init
+  #   niv add input-output-hk/haskell.nix -n haskellNix
+
+  # Fetch the haskell.nix commit we have pinned with Niv
+  haskellNix = import sources.haskellNix {};
+  # If haskellNix is not found run:
+  #   niv add input-output-hk/haskell.nix -n haskellNix
+
+  # Import nixpkgs and pass the haskell.nix provided nixpkgsArgs
+  pkgs = import
+    # haskell.nix provides access to the nixpkgs pins which are used by our CI,
+    # hence you will be more likely to get cache hits when using these.
+    # But you can also just use your own, e.g. '<nixpkgs>'.
+    haskellNix.sources.nixpkgs-2009
+    # These arguments passed to nixpkgs, include some patches and also
+    # the haskell.nix functionality itself as an overlay.
+    haskellNix.nixpkgsArgs;
+
+  # Help it find the non-Haskell dependency libchallenge_bypass_ristretto_ffi.
+  pkgs' = pkgs.appendOverlays [(import ./oldnix/overlay.nix)];
+in pkgs'.haskell-nix.project {
+  # 'cleanGit' cleans a source directory based on the files known by git
+  src = pkgs.haskell-nix.haskellLib.cleanGit {
+    name = "PaymentServer";
+    src = ./.;
+  };
+}
