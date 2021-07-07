bc2dcee3a131084ce99b6069b5202ef0e5af1b2b
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2021-07-07 13:48:07 -0400

    try with the Haskell.nix-recommended shell.nix

--- shell.nix
+++ shell.nix
@@ -1,6 +1,9 @@
-{ pkgs ? import <nixpkgs> { } }:
-pkgs.mkShell {
-  buildInputs = [
-    pkgs.stack
-  ];
-}
+# shell.nix
+let
+  project = import ./default.nix;
+in
+  project.shellFor {
+    # Prevents cabal from choosing alternate plans, so that
+    # *all* dependencies are provided by Nix.
+    exactDeps = true;
+  }
