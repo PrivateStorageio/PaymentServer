cfe4189facd215aca299347b2856254f649f9eef
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2021-07-12 19:44:18 -0400

    Let challenge-bypass-ristretto pick its own nixpkgs

--- nix/challenge-bypass-ristretto.nix
+++ nix/challenge-bypass-ristretto.nix
@@ -3,4 +3,4 @@
 let
   src = import ./challenge-bypass-ristretto-repo.nix;
 in
-  callPackage "${src}/challenge-bypass-ristretto.nix" { }
+  import "${src}/default-challenge-bypass-ristretto-ffi.nix" { }
