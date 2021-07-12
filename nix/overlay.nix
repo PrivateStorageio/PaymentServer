ad67fd2aafd6e524411673eb4b73fce1bfc9c31b
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2021-07-12 10:31:14 -0400

    make the name mean the library

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,3 +1,3 @@
 self: super: {
-  libchallenge_bypass_ristretto_ffi = super.callPackage ./challenge-bypass-ristretto.nix { };
+  libchallenge_bypass_ristretto_ffi = (super.callPackage ./challenge-bypass-ristretto.nix { }).lib;
 }
