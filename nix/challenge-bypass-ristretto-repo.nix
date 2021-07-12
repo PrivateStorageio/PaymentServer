22db0201e089c690987c2e85c98f4742ac947cf6
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2021-07-12 14:20:25 -0400

    use a nicer name for the revision we want

--- nix/challenge-bypass-ristretto-repo.nix
+++ nix/challenge-bypass-ristretto-repo.nix
@@ -4,6 +4,6 @@ in
   pkgs.fetchFromGitHub {
     owner = "LeastAuthority";
     repo = "python-challenge-bypass-ristretto";
-    rev = "0c4af863a7485bb99523c4f5ee978ec6699d78e3";
+    rev = "v2021.07.12";
     sha256 = "16af1qmx7srhvcc936x7hl2bz50hafm39311dbzqam9ms1i5q89j";
   }
