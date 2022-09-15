0ea003723c43bc77d69e560308c4d25fb53c7b30
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2022-09-15 15:17:31 -0400

    attempt to update haskell.nix all the way, adding the extra pkg-config stuff

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -4,4 +4,13 @@ self: super: {
   # the derivation because that's where the nixpkgs Rust tooling seems to put
   # all of the good files nowadays.
   libchallenge_bypass_ristretto_ffi = (super.callPackage ./challenge-bypass-ristretto.nix { }).lib;
+
+  # And map the name of the package in the pkg-config-resolution parts of
+  # Haskell.nix so that when we ask for it by name from the cabal file it can
+  # be resolved.
+  haskell-nix = super.haskell-nix // {
+    extraPkgconfigMappings = super.haskell-nix.extraPkgconfigMappings // {
+      "libchallenge_bypass_ristretto_ffi" = [ "libchallenge_bypass_ristretto_ffi" ];
+    };
+  };
 }
