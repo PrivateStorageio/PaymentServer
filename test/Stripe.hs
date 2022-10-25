ec6584b5893ffd5cfdf2d014cbaa3f1427288584
Florian Sesser <florian@private.storage>
2022-10-25 16:52:32 +0000

    start writing a test for the webhook endpoint

--- test/Stripe.hs
+++ test/Stripe.hs
@@ -47,13 +47,21 @@ import Data.Aeson
   ( decode
   )
 
+import Web.Stripe.Client
+  ( StripeConfig(StripeConfig)
+  , StripeKey(StripeKey)
+  )
+  
 import Web.Stripe.Types
   ( Currency(USD, AED)
   , ChargeId(ChargeId)
   )
-
+import Network.HTTP.Types
+  ( status200
+  )
 import Network.Wai.Test
   ( SRequest(SRequest)
+  , SResponse(simpleStatus)
   , runSession
   , request
   , srequest
@@ -78,7 +86,7 @@ import PaymentServer.Processors.Stripe
   , Acknowledgement(Ok)
   , Failure(Failure)
   , charge
-
+  , webhookServer
   )
 
 import PaymentServer.Issuer
@@ -102,6 +110,7 @@ tests :: TestTree
 tests = testGroup "Stripe"
   [ chargeTests
   , corsTests
+  , webhookTests
   ]
 
 corsTests :: TestTree
@@ -218,3 +227,34 @@ chargeTests =
   where
     token = "foo"
     voucher = "bar"
+
+-- TODO
+-- Make "charge.succeeded" fail with a good error message
+-- Make "charge.succeeded" pass
+-- Prevent replay attacks? https://stripe.com/docs/webhooks/signatures#replay-attacks
+-- Check network origin? https://stripe.com/docs/ips#webhook-notifications
+-- Check the Stripe signature
+
+
+webhookTests :: TestTree
+webhookTests =
+  testGroup "The Stripe charge web hook"
+  [ testCase "charge.succeeded" $ do
+      db <- memory
+      let stripeKey = StripeKey ""
+      let stripeConfig = StripeConfig stripeKey Nothing
+      let origins = []
+      let redemptionConfig = RedemptionConfig 16 1024 trivialIssue
+      let app = paymentServerApp origins stripeConfig redemptionConfig db
+
+      response <- (flip runSession) app $ srequest theSRequest
+      assertEqual "The response is 200" status200 (simpleStatus response)
+  ]
+  where
+    path = "/v1/stripe/webhook"
+    theRequest = setPath defaultRequest
+      { requestMethod = "POST"
+      , requestHeaders = [("content-type", "application/json")]
+      } path
+    theSRequest = SRequest theRequest body
+    body = "{\"api_version\":\"2022-08-01\",\"created\":1665593127,\"data\":{\"object\":{\"amount\":250,\"amount_captured\":250,\"amount_refunded\":0,\"application\":null,\"application_fee\":null,\"application_fee_amount\":null,\"balance_transaction\":\"txn_3Ls83eLswFpehDNg0dFvPaKv\",\"billing_details\":{\"address\":{\"city\":null,\"country\":\"DE\",\"line1\":null,\"line2\":null,\"postal_code\":null,\"state\":null},\"email\":\"a@b.d\",\"name\":\"asdfasf\",\"phone\":null},\"calculated_statement_descriptor\":\"Stripe\",\"captured\":true,\"created\":1665593127,\"currency\":\"usd\",\"customer\":null,\"description\":null,\"destination\":null,\"dispute\":null,\"disputed\":false,\"failure_balance_transaction\":null,\"failure_code\":null,\"failure_message\":null,\"fraud_details\":{},\"id\":\"ch_3Ls83eLswFpehDNg0WVw0vTa\",\"invoice\":null,\"livemode\":false,\"metadata\":{},\"object\":\"charge\",\"on_behalf_of\":null,\"order\":null,\"outcome\":{\"network_status\":\"approved_by_network\",\"reason\":null,\"risk_level\":\"normal\",\"risk_score\":21,\"seller_message\":\"Payment complete.\",\"type\":\"authorized\"},\"paid\":true,\"payment_intent\":\"pi_3Ls83eLswFpehDNg0b2mAFUW\",\"payment_method\":\"pm_1Ls83dLswFpehDNgpYAGL3j9\",\"payment_method_details\":{\"card\":{\"brand\":\"mastercard\",\"checks\":{\"address_line1_check\":null,\"address_postal_code_check\":null,\"cvc_check\":\"pass\"},\"country\":\"US\",\"exp_month\":12,\"exp_year\":2023,\"fingerprint\":\"DoAWRfUcyOfJupbL\",\"funding\":\"credit\",\"installments\":null,\"last4\":\"4444\",\"mandate\":null,\"network\":\"mastercard\",\"three_d_secure\":null,\"wallet\":null},\"type\":\"card\"},\"receipt_email\":null,\"receipt_number\":null,\"receipt_url\":\"https://pay.stripe.com/receipts/payment/CAcaFwoVYWNjdF8xTGZORGFMc3dGcGVoRE5nKKjem5oGMgZo4m-xDMM6LBadftys-t7FIeo23hfQKTAtYI3zpLwmJb_3-A6VqCpIGjfmpkWUwCDQC38M\",\"refunded\":false,\"refunds\":{\"data\":[],\"has_more\":false,\"object\":\"list\",\"total_count\":0,\"url\":\"/v1/charges/ch_3Ls83eLswFpehDNg0WVw0vTa/refunds\"},\"review\":null,\"shipping\":null,\"source\":null,\"source_transfer\":null,\"statement_descriptor\":null,\"statement_descriptor_suffix\":null,\"status\":\"succeeded\",\"transfer_data\":null,\"transfer_group\":null}},\"id\":\"evt_3Ls83eLswFpehDNg0dmzogyf\",\"livemode\":false,\"object\":\"event\",\"pending_webhooks\":2,\"request\":{\"id\":\"req_F8pjOORr12gJT9\",\"idempotency_key\":\"8fdd25c9-cb73-4807-973f-f0b21d8bb7cc\"},\"type\":\"charge.succeeded\"}"
